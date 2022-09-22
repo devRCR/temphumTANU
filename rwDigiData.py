7795f0ace16fdcd2fe51884f53972b03772fbab8
devRCR <renzo.chan@outlook.com>
2022-09-22 13:06:57 -0500

    test

--- rwDigiData.py
+++ rwDigiData.py
@@ -69,7 +69,7 @@ while(True):
                     
             try:
                 payload = xbee_message.data.decode("utf8")   
-                print(xbee_message.data + ' ' + remoteID)
+                print(str(xbee_message.data) + ' ' + remoteID)
             except UnicodeDecodeError:
                 #payload = auxPayload
                 print ('utf-8 codec can not decode data'+remoteID)           
